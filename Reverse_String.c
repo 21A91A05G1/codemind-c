ec711cd700bc3d1da6a87c6b4a8dbdaf1e6bc280
21A91A05G1 <97424960+21A91A05G1@users.noreply.github.com>
2022-06-21 21:29:53 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100];     int i,j,len,startindex,endindex;     scanf("%[^ ]s",str);     len=strlen(str);     endindex = len - 1;
        for(i= len - 1;i>=0;i--)
        {
            if(str[i] == ' '|| i==0)
            {
                if(i==0)
                {
                    startindex = 0;
                }
                else
                {
                    startindex = i+1;
                }
                for(j=startindex;j<=endindex;j++)
                {
                    printf("%c",str[j]);
                }
                endindex = i-1;
                printf(" ");
            }
        }
        return 0;
    }

--- Reverse_String.c
+++ Reverse_String.c
@@ -7,24 +7,25 @@ int main()
     scanf("%[^
 ]s",str);
     len=strlen(str);
-    endindex=len - 1;
-    for(i = len-1;i>=0;i--)
+    endindex = len - 1;
+
+    for(i= len - 1;i>=0;i--)
     {
-        if(str[i]==' '|| i==0)
+        if(str[i] == ' '|| i==0)
         {
-            if(i == 0)
+            if(i==0)
             {
-                startindex=0;
+                startindex = 0;
             }
             else
             {
-                startindex=i+1;
+                startindex = i+1;
             }
             for(j=startindex;j<=endindex;j++)
             {
                 printf("%c",str[j]);
             }
-            endindex=i-1;
+            endindex = i-1;
             printf(" ");
         }
     }
