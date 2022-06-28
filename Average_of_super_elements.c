b92d52570e43261eeb356ee1a734345a713cac54
21A91A05G1 <97424960+21A91A05G1@users.noreply.github.com>
2022-06-28 15:22:33 +0530

    #include<stdio.h> int main() {     int n,arr[100],sum=0,items=0;     int skip[100];     double res;     scanf("%d",&n);     for(int i = 0;i < n;++i)     {         scanf("%d",&arr[i]);         skip[i] = 0;     }     for(int i=0;i<n;i++)     {         int count = 0;         if(skip[i]) continue;         for(int j=0;j<n;j++)         {             if(arr[i]==arr[j])             {                 count++;                 skip[j] = 1;             }         }         if(arr[i] == count)         {             sum += arr[i];             items++;         }     }     if(items == 0)     {         printf("-1");     }     else     {         res = ((double)sum)/items;         printf("%.2f",res);     }     printf(" ");     return 0; }

---
+++ Average_of_super_elements.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],sum=0,items=0;
+    int skip[100];
+    double res;
+    scanf("%d",&n);
+    for(int i = 0;i < n;++i)
+    {
+        scanf("%d",&arr[i]);
+        skip[i] = 0;
+    }
+    for(int i=0;i<n;i++)
+    {
+        int count = 0;
+        if(skip[i]) continue;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                count++;
+                skip[j] = 1;
+            }
+        }
+        if(arr[i] == count)
+        {
+            sum += arr[i];
+            items++;
+        }
+    }
+    if(items == 0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        res = ((double)sum)/items;
+        printf("%.2f",res);
+    }
+    printf("
+");
+    return 0;
+}
