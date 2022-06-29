ed2467659e34ca5fd8f115b620139546f8ea7a99
21A91A05G1 <97424960+21A91A05G1@users.noreply.github.com>
2022-06-29 21:55:25 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100];     int count=0;     scanf("%[^ ]s",str);     for(int i=0;str[i]!=NULL;i++)     {
            count++;
        }
        printf("%d",count);
    }

---
+++ characters_in_a_string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    int count=0;
+    scanf("%[^
+]s",str);
+    for(int i=0;str[i]!=NULL;i++)
+    {
+        
+        count++;
+    }
+    printf("%d",count);
+}
