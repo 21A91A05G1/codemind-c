77e3996034fd5beded3cb149d42becd74859044a
21A91A05G1 <97424960+21A91A05G1@users.noreply.github.com>
2022-06-29 20:15:05 +0530

    #include<stdio.h> #include<math.h>
    int main()
    {
        int n,arr[100],c=0,i,d;
        scanf("%d",&n);
        for(i=0;i<n;i++)
        {
            scanf("%d",&arr[i]);
        }
        for(i=0;i<n;i++)
        {
            d=(int)log10(arr[i])+1;
            if(d%2==0)
            {
                c++;
            }
        }
        printf("%d",c);
    }

---
+++ Find_Numbers_with_Even_Number_of_Digits.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int n,arr[100],c=0,i,d;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        d=(int)log10(arr[i])+1;
+        if(d%2==0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
+        
